7052bb22cc8d6f7efddbdc85b5f482a022576eda
sarvagya005 <sarvagyasingh4516@gmail.com>
2024-09-19 01:21:02 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["https://main--leaderboard-frontend.netlify.app/"],
+    origin: ["https://main--leaderboard-frontend.netlify.app"],
     credentials:true,
   })
 );
